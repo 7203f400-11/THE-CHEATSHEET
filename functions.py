566956a4cee8ad7dff1907e8a6aaf6e0c883f9ea
Danish Potatohead <112283051+7203f400-11@users.noreply.github.com>
2022-10-20 18:15:47 -0500

    Update 0.5

--- functions.py
+++ functions.py
@@ -1,6 +1,21 @@
 class MathFunctions(self):
   def __init__(self):
     return 'OPERATING'
-    
+  def percentval(self):
+    def inputs():
+        startval = input("Enter original value\n")
+        endval = input("Enter new value\n")
+        return float(startval), float(endval)
+    def change(s, e):
+        change = e-s
+        fraction = change/s
+        return fraction*100
+    s, e = inputs()
+    out = change(s, e)
+    approximate = round(change(s, e))
+    if out == approximate:
+        return f'Exact: {out}%'
+    else:
+        return (f'Exact(almost): {out}', f'Approximate: {approximate}%')
     
    
--- main.py
+++ main.py
@@ -1,4 +1,39 @@
+import functions.py as functions #import functions
+import csv #command base handling
+#from colorama import Fore, Back, init #cosmetics
+commandpath = "7203f400-11/THE-CHEATSHEET/commands.csv"
+def loadfiles():
+  '''
+  Loads command base
+  '''
+  with open(commandpath) as commands:
+    commandbase = [row["COMMAND"] for row in commands]
+  return (commandbase,)
+def dash(n):
+  '''
+  Takes command input
+  '''
+  while True:
+    try:
+      command = input("Enter your command\n")
+      return cbase[command]
+      break
+    except ValueError:
+      print("\nInvalid Command")
+      continue
+def container():
+  '''
+  Initializes an instance of the functions file
+  '''
+  c = functions.MathFunctions()
+  return (c,)
 def main():
-  
+  '''
+  The main script
+  '''
+  cbase = loadfiles()
+  daman = container()
+  command = dash(cbase)
+  c.command()
 if __name__ == "__main__":
   main()
